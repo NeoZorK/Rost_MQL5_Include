685b603e46f5b7004a59ba0822e521b5cb9f2f25
Ростислав Щербина’; GIT_AUTHOR_EMAIL=neozork@protonmail.com’ <neozork@protonmail.com>
2024-09-24 20:30:21 +0300

    add grn.sh

---
+++ grn.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Script Modified by Shcherbyna Rostyslav 24.09.2024
+# Rename Autor and Email to Corrected One in ALL GIT Repositry History
+#
+# using:
+# 1. Copy this file to GIT Repository folder
+# 2. run it from terminal : bash grn.sh
+#
+# Use this if backup cannot rewrite self :
+# bash git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
+
+git filter-branch --env-filter '
+OLD_EMAIL="neozork@gmail.com"
+CORRECT_NAME="Щербина Ростислав"
+CORRECT_EMAIL="neozork@protonmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
