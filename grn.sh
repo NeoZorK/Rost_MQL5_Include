55c8e0c213cdcfcbcf242c3d80384d394e3e51a0
Ростислав Щербина’; GIT_AUTHOR_EMAIL=neozork@protonmail.com’ <neozork@protonmail.com>
2024-09-24 20:36:57 +0300

    neo@gmail.com

--- grn.sh
+++ grn.sh
@@ -10,7 +10,7 @@
 # bash git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
 
 git filter-branch --env-filter '
-OLD_EMAIL="neozork@pm.me"
+OLD_EMAIL="neo@gmail.com"
 CORRECT_NAME="Щербина Ростислав"
 CORRECT_EMAIL="neozork@protonmail.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
